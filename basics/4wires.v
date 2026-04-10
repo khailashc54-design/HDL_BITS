e3184b8fa311ecdfaf4b8d3747ea481822869e75
khailashc54-design <khailashc54@gmail.com>
2026-04-10 19:28:02 +0530

    Add top_module with wire connections
    Created a module with 3 inputs and 4 outputs that behaves like wires.

---
+++ basics/4wires.v
@@ -0,0 +1,18 @@
+//Create a module with 3 inputs and 4 outputs that behaves like wires that makes these connections:
+module top_module( 
+    input a,b,c,
+    output w,x,y,z );
+    assign w=a;
+    assign z=c;
+    assign y=b;
+    assign x=b;
+
+endmodulemodule top_module( 
+    input a,b,c,
+    output w,x,y,z );
+    assign w=a;
+    assign z=c;
+    assign y=b;
+    assign x=b;
+
+endmodule
