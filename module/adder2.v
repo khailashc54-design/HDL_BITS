f9923e52212025e7afe3927fc26a5acc4ec55f42
khailashc54-design <khailashc54@gmail.com>
2026-04-14 12:12:41 +0530

    Create adder2.v

---
+++ module/adder2.v
@@ -0,0 +1,17 @@
+module top_module (
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);  
+    wire c;
+    add16 f1(.a(a[15:0]),.b(b[15:0]),.sum(sum[15:0]),.cout(c),.cin(1'b0));
+    add16 f2(.a(a[31:16]),.b(b[31:16]),.sum(sum[31:16]),.cin(c),.cout());
+    
+
+endmodule
+
+module add1 ( input a, input b, input cin,   output sum, output cout );
+// Full adder module here
+    assign sum  = a ^ b ^ cin;
+    assign cout = (a & b) | (b & cin) | (a & cin);
+endmodule
