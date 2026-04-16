0f92b6e7753ea8cf63bef5029c22dbc73ef9c538
khailashc54-design <khailashc54@gmail.com>
2026-04-16 09:21:25 +0530

    Create popcount.v

---
+++ circuits/combinational/basic gates/popcount.v
@@ -0,0 +1,12 @@
+module top_module( 
+    input [2:0] in,
+    output [1:0] out );
+    integer i;
+    always@(*)begin
+        out =1'b0;
+        for (i=0;i<3;i=i+1)begin
+            out=out+in[i];
+        end
+    end
+
+endmodule
