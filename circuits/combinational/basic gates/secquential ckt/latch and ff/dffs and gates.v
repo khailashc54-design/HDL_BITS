32b94e88b123fa71de6f5cadb1c699b5f8e51a80
khailashc54-design <khailashc54@gmail.com>
2026-04-20 17:23:51 +0530

    Add D flip-flop and gates implementation

---
+++ circuits/combinational/basic gates/secquential ckt/latch and ff/dffs and gates.v
@@ -0,0 +1,9 @@
+module top_module (input clk,input x,output z);
+    wire q1,q2,q3;
+    always @(posedge clk) begin
+        q1<=q1^x;
+        q2<=(~q2)&x;
+        q3<=(~q3)|x;
+    end
+    assign z=~(q1|q2|q3);
+endmodule
